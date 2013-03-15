7d2a86d1e67c6220b876d3d2c6d5bfe197ca6271
Mike Joseph <mike.joseph5@gmail.com>
2013-03-14 22:28:33 -0400

    API docs

---
+++ docs/tree.js
@@ -0,0 +1,13 @@
+var tree = [["MODL","MODL.html","",
+                [["GivingImpact","MODL\/GivingImpact.html","",
+                [["Model","MODL\/GivingImpact\/Model.html","",
+                [["Campaign","MODL\/GivingImpact\/Model\/Campaign.html"," < Model",
+                [        ]],["Donation","MODL\/GivingImpact\/Model\/Donation.html"," < Model",
+                [        ]],["Opportunity","MODL\/GivingImpact\/Model\/Opportunity.html"," < Model",
+                [        ]],["Stats","MODL\/GivingImpact\/Model\/Stats.html"," < Model",
+                [        ]]        ]],["Exception","MODL\/GivingImpact\/Exception.html"," < Exception",
+                [        ]],["Model","MODL\/GivingImpact\/Model.html","",
+                [        ]],["RestClient","MODL\/GivingImpact\/RestClient.html","",
+                [        ]]        ]],["GivingImpact","MODL\/GivingImpact.html","",
+                [        ]]        ]]        ]
+
