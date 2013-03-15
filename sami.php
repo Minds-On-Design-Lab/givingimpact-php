6ae0617f4efc5b6c3c2f9b0ef665d203f18a6632
Mike Joseph <mike.joseph5@gmail.com>
2013-03-14 22:30:35 -0400

    sami config

---
+++ sami.php
@@ -0,0 +1,13 @@
+<?php
+
+$i = Symfony\Component\Finder\Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in(__DIR__.'/MODL');
+
+return new Sami\Sami($i, array(
+    'title'                => 'Giving Impact API',
+    'build_dir'            => __DIR__.'/build',
+    'cache_dir'            => __DIR__.'/cache',
+    'default_opened_level' => 2
+));
