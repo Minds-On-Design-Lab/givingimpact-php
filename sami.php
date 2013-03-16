4e918e8b4081144d7521bae98641a0f9ee6c7e0c
Mike Joseph <mike.joseph5@gmail.com>
2013-03-15 21:23:48 -0400

    updated build path

--- sami.php
+++ sami.php
@@ -7,7 +7,7 @@ $i = Symfony\Component\Finder\Finder::create()
 
 return new Sami\Sami($i, array(
     'title'                => 'Giving Impact API',
-    'build_dir'            => __DIR__.'/build',
+    'build_dir'            => __DIR__.'/docs',
     'cache_dir'            => __DIR__.'/cache',
     'default_opened_level' => 2
 ));
